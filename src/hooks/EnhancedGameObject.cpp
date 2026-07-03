89cdd8b0e2b001f9ede94820a0eeacda3cd91cb7
mosquared2055-star <mosquared2055@gmail.com>
2026-07-03 16:19:03 +0300

    Initial commit with Android patches

---
+++ src/hooks/EnhancedGameObject.cpp
@@ -0,0 +1,35 @@
+#include <Geode/Geode.hpp>
+#include <bot/updater.hpp>
+#ifdef GEODE_IS_WINDOWS
+#include <safetyhook.hpp>
+#endif
+
+#include "bot/bot.hpp"
+#include "physics/collisions.hpp"
+#include "trajectory/trajectory.hpp"
+
+using namespace geode::prelude;
+
+#include <Geode/modify/EnhancedGameObject.hpp>
+
+struct SLEnhancedGameObject : Modify<SLEnhancedGameObject, EnhancedGameObject> {
+    void activatedByPlayer(PlayerObject* player) {
+        auto bot = Bot::get();
+        if (bot->trajectory().isFakePlayer(player)) {
+            phys::activateForTrajectory((EffectGameObject*)this, player);
+        } else {
+            EnhancedGameObject::activatedByPlayer(player);
+
+            // const GJBaseGameLayer* gjbgl = GJBaseGameLayer::get();
+            // const bool activated = hasBeenActivatedByPlayer(gjbgl->m_player1)
+            // &&
+            //     (!gjbgl->m_gameState.m_isDualMode ||
+            //     hasBeenActivatedByPlayer(gjbgl->m_player2));
+            // if (activated && bot->updater().m_layoutMode->inner()) {
+            //     m_opacityMod = 0.5;
+            //     m_opacityMod2 = 0.5;
+            //     m_hasNoEffects = true;
+            // }
+        }
+    }
+};
